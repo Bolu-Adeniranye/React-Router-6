23a565dc0fe87a7e8682f880289d38c6377e7b0b
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-07-30 17:26:11 +0100

    Mirage.js is weird

--- Router6/src/App.jsx
+++ Router6/src/App.jsx
@@ -1,12 +1,22 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './components/Home'
-import About from './components/About.'
-
+import About from './components/About'
+import Vans from './components/Vans'
 export default function App() {
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element={<About />} />    
-      </Routes>
-    </BrowserRouter>
-}
+    return (
+      <BrowserRouter>
+        <header>
+          <Link className="site-logo" to="/">#Van4Life</Link>
+          <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Check Out Our Vans</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
---
+++ Router6/src/components/Vans.jsx
@@ -0,0 +1,8 @@
+// import { Link } from "react-router-dom"
+
+
+export default function Vans() {
+    return(
+    <h1>Hi</h1>
+    )
+}
