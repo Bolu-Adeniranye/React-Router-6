411b7648d5e5f844e18c0bbc546115346aeb214b
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-07-30 20:17:50 +0100

    Will finish tommorrow

--- Router6/src/components/HostLayout.jsx
+++ Router6/src/components/HostLayout.jsx
@@ -4,9 +4,9 @@ export default function HostLayout() {
     return (
         <>
             <nav className="host-nav">
-                <Link to="/host/dashboard">Dashboard</Link>
+            <Link to="/host">Dashboard</Link>
                 <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <Link to="/host/reviews">Reviews</Link> 
             </nav>
             <Outlet />
         </>
