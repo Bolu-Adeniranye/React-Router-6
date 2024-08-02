38158be772de2beed1ba5f73f3c0113b3d9f7f54
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-08-02 11:01:41 +0100

    Hmm

--- Router6/src/components/Vans/VanDetail.jsx
+++ Router6/src/components/Vans/VanDetail.jsx
@@ -1,8 +1,9 @@
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, useLocation } from "react-router-dom"
 import { useEffect, useState } from "react"
 
 export default function VanDetail() {
     const params = useParams()
+    const location = useLocation()
     const [van, setVan] = useState(null)
 
     useEffect(() => {
@@ -11,13 +12,15 @@ export default function VanDetail() {
             .then(data => setVan(data.vans))
     }, [params.id])
 
+    const search = location.state?.search || ""
+    const type = location.state?.type || "all"
     return (
         <div className="van-detail-container">
             <Link
-                to=".."
+                to={`..${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             
             {van ? (
                 <div className="van-detail">
